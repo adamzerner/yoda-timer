97a78186f6a6227252bd288e1a3266abbc906171
Adam Zerner <adamzerner@protonmail.com>
2022-06-10 23:36:46 -0700

    Logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
-  return <section>Hello World</section>;
+  const [isActive, setIsActive] = useState(false);
+  const [numSecondsLeft, setNumSecondsLeft] = useState(60 * 5);
+  const start = () => {
+    setIsActive(true);
+  };
+  const pause = () => alert("Pause a Yoda Timer, one shall not.");
+  const reset = () => {
+    setIsActive(false);
+    setNumSecondsLeft(60 * 5);
+  };
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setNumSecondsLeft((n) => n - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  return (
+    <section>
+      <section>{numSecondsLeft}</section>
+      <section>
+        <button type="button" onClick={start} disabled={isActive}>
+          Start
+        </button>
+        <button type="button" onClick={pause}>
+          Pause
+        </button>
+        <button type="button" onClick={reset} disabled={!isActive}>
+          Reset
+        </button>
+      </section>
+      <section>Yoda</section>
+    </section>
+  );
 };
 
 export default App;
