852f0e3d1fae7d1998e6b8c60766687f48c513e5
Adam Zerner <adamzerner@protonmail.com>
2022-06-18 18:22:53 -0700

    Fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import splitbee from "./splitbee.js";
+import "./splitbee.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
